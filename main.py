519ab12fb03c0ced1f25deedfef6b9fa046a377a
justinlachap <justinlachapel@gmail.com>
2022-05-26 15:59:42 -0400

    reduced computational complexity

--- main.py
+++ main.py
@@ -207,8 +207,8 @@ def draw_grid(surface, row, col):
     sy = top_left_y
     for i in range(row):
         pygame.draw.line(surface, (128,128,128), (sx, sy+ i*30), (sx + play_width, sy + i * 30))  # horizontal lines
-        for j in range(col):
-            pygame.draw.line(surface, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + play_height))  # vertical lines
+    for j in range(col):
+        pygame.draw.line(surface, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + play_height))  # vertical lines
 
 
 def clear_rows(grid, locked):
